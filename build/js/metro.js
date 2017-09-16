ac6f1a166460c54b29d584e56cc039c973688a7c
Sergey Pimenov <sergey@pimenov.com.ua>
2017-09-16 10:36:30 +0300

    add toast

--- build/js/metro.js
+++ build/js/metro.js
@@ -2728,6 +2728,30 @@ var Textarea = {
 };
 
 Metro.plugin('textarea', Textarea);
+// Source: js/plugins/toast.js
+var toast = {
+    create: function(message, callback, timeout, cls){
+        var toast = $("<div>").addClass("toast").html(message).appendTo($("body")).hide();
+        var width = toast.outerWidth();
+        var height = toast.outerHeight();
+        timeout = timeout || METRO_TIMEOUT;
+
+        toast.css({
+            'left': '50%',
+            'margin-left': -(width / 2),
+            'border-radius': height/2
+        }).addClass(cls).fadeIn(METRO_ANIMATION_DURATION);
+
+        setTimeout(function(){
+            toast.fadeOut(METRO_ANIMATION_DURATION, function(){
+                toast.remove();
+                Utils.callback(callback);
+            });
+        }, timeout);
+    }
+};
+
+$.Metro['toast'] = toast;
 // Source: js/plugins/validator.js
 var Validator = {
     init: function( options, elem ) {
