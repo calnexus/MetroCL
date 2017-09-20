ca5ab619abfc4b4984ac7e4f8f2989bfdb267992
Sergey Pimenov <sergey@pimenov.com.ua>
2017-09-20 13:00:36 +0300

    add interactive check to validator, change patterns for email and url for validator funcs

--- build/js/metro.js
+++ build/js/metro.js
@@ -2892,6 +2892,8 @@ var Validator = {
         return this;
     },
     options: {
+        submitTimeout: 200,
+        interactiveCheck: false,
         onBeforeSubmit: function(){},
         onSubmit: function(){},
         onError: function(){},
@@ -2938,6 +2940,11 @@ var Validator = {
                     input.addClass("required");
                 }
             }
+            if (o.interactiveCheck === true) {
+                input.on("propertychange change keyup input paste", function () {
+                    that._check(this);
+                });
+            }
         });
 
         this._onsubmit = null;
@@ -2952,68 +2959,88 @@ var Validator = {
         };
     },
 
+    _check: function(el, result, events){
+        var that = this, o = this.options;
+        var this_result = true;
+        var input = $(el);
+        var control = this.is_control(input);
+        var funcs = input.data('validate') !== undefined ? String(input.data('validate')).split(",").map(function(s){return s.trim();}) : [];
+
+        if (input.is(":disabled")) return;
+
+        if (control) {
+            input.parent().removeClass("invalid valid");
+        } else {
+            input.removeClass("invalid valid");
+        }
+
+        $.each(funcs, function(){
+            if (this_result === false) return;
+            var rule = this.split("=");
+            var f, a;
+
+            f = rule[0]; rule.shift();
+            a = rule.join("=");
+
+            if (f === 'compare') {
+                a = that.element[0].elements[a].value;
+            }
+            this_result = that._funcs[f](input.val(), a);
+            if (result !== undefined) {
+                result.val += this_result ? 0 : 1;
+            }
+        });
+
+        if (this_result === false) {
+            if (control) {
+                input.parent().addClass("invalid")
+            } else {
+                input.addClass("invalid")
+            }
+
+            if (events === true) {
+                Utils.exec(o.onError, [input, input.val()]);
+            }
+
+        } else {
+            if (control) {
+                input.parent().addClass("valid")
+            } else {
+                input.addClass("valid")
+            }
+
+            if (events === true) {
+                Utils.exec(o.onValid, [input, input.val()]);
+            }
+        }
+    },
+
     _submit: function(){
         var that = this, element = this.element, o = this.options;
         var inputs = element.find("[data-validate]");
         var submit = element.find(":submit").attr('disabled', 'disabled').addClass('disabled');
-        var result = 0;
+        var result = {
+            val: 0
+        };
 
         $.each(inputs, function(){
-            var this_result = true;
-            var input = $(this);
-            var control = that.is_control(input);
-            var funcs = input.data('validate') !== undefined ? String(input.data('validate')).split(",").map(function(s){return s.trim();}) : [];
-
-            if (input.is(":disabled")) return;
-
-            if (control) {
-                input.parent().removeClass("invalid valid");
-            } else {
-                input.removeClass("invalid valid");
-            }
-
-            $.each(funcs, function(){
-                if (this_result === false) return;
-                var f = this.split("=")[0], a = this.split("=")[1];
-                if (f === 'compare') {
-                    a = that.element[0].elements[a].value;
-                }
-                this_result = that._funcs[f](input.val(), a);
-                result += this_result ? 0 : 1;
-            });
-
-            if (this_result === false) {
-                if (control) {
-                    input.parent().addClass("invalid")
-                } else {
-                    input.addClass("invalid")
-                }
-
-                Utils.exec(o.onError, [input, input.val()]);
-
-            } else {
-                if (control) {
-                    input.parent().addClass("valid")
-                } else {
-                    input.addClass("valid")
-                }
-
-                Utils.exec(o.onValid, [input, input.val()]);
-            }
+            that._check(this, result, true);
         });
 
         submit.removeAttr("disabled").removeClass("disabled");
 
         element[0].action = this._action;
 
-        result += Utils.exec(o.onBeforeSubmit, [element[0]]) === false ? 1 : 0;
+        result.val += Utils.exec(o.onBeforeSubmit, [element[0]]) === false ? 1 : 0;
 
-        if (result === 0) {
-            Utils.exec(o.onSubmit, [element[0]]);
-            if (this._onsubmit !==  null) Utils.exec(this._onsubmit);
+        if (result.val === 0) {
+            setTimeout(function(){
+                Utils.exec(o.onSubmit, [element[0]]);
+                if (that._onsubmit !==  null) Utils.exec(that._onsubmit);
+            }, o.submitTimeout);
         }
 
-        return result === 0;
+        return result.val === 0;
     },
 
     _funcs: {
@@ -3063,10 +3090,10 @@ var Validator = {
             return Number(val) <= Number(max_value);
         },
         email: function(val){
-            return /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i.test(val);
+            return /^[a-z0-9\u007F-\uffff!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9\u007F-\uffff!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z]{2,}$/i.test(val);
         },
         url: function(val){
-            return /^(?:[a-z]+:)?\/\//i.test(val);
+            return /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(val);
         },
         date: function(val){
             return (new Date(val) !== "Invalid Date" && !isNaN(new Date(val)));
