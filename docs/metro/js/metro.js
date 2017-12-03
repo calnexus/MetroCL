0fc4169c3d91b4e59139e07ffd270926167adc21
Sergey Pimenov <sergey@pimenov.com.ua>
2017-12-03 14:01:14 +0200

    add tiles

--- docs/metro/js/metro.js
+++ docs/metro/js/metro.js
@@ -9682,6 +9682,49 @@ var Textarea = {
 };
 
 Metro.plugin('textarea', Textarea);
+// Source: js/plugins/tiles.js
+var Tile = {
+    init: function( options, elem ) {
+        this.options = $.extend( {}, this.options, options );
+        this.elem  = elem;
+        this.element = $(elem);
+
+        this._setOptionsFromDOM();
+        this._create();
+
+        return this;
+    },
+
+    options: {
+        onTileCreate: Metro.noop
+    },
+
+    _setOptionsFromDOM: function(){
+        var that = this, element = this.element, o = this.options;
+
+        $.each(element.data(), function(key, value){
+            if (key in o) {
+                try {
+                    o[key] = $.parseJSON(value);
+                } catch (e) {
+                    o[key] = value;
+                }
+            }
+        });
+    },
+
+    _create: function(){
+        var that = this, element = this.element, o = this.options;
+
+        Utils.exec(o.onCreate, [element]);
+    },
+
+    changeAttribute: function(attributeName){
+
+    }
+};
+
+Metro.plugin('tile', Tile);
 // Source: js/plugins/toast.js
 var Toast = {
     create: function(message, callback, timeout, cls){
