a58354c04ee636b0aea1a5a4335577a0fbfc047b
Sergey Pimenov <sergey@pimenov.com.ua>
2017-12-09 21:33:55 +0200

    upd adv

--- Gruntfile.js
+++ Gruntfile.js
@@ -171,11 +171,33 @@ module.exports = function(grunt) {
                     patterns: [
                         {
                             match: /<!-- ads-html -->/g,
-                            replacement: '<div style="margin:10px 0"><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1632668592742327" data-ad-slot="8347181909" data-ad-format="auto"></ins></div>'
+                            replacement: '' +
+                            '<div style="margin:10px 0">' +
+                            '<!-- Metro UI Responsive 1 -->\n' +
+                            '<ins class="adsbygoogle" ' +
+                            '     style="display:block" ' +
+                            '     data-ad-client="ca-pub-1632668592742327" ' +
+                            '     data-ad-slot="8347181909" ' +
+                            '     data-ad-format="auto"></ins>' +
+                            '</div>' +
+                            '<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>'
+                        },
+                        {
+                            match: /<!-- ads-side -->/g,
+                            replacement: '' +
+                            '<div style="margin:10px 0">' +
+                            '<!-- Metro 4 side -->\n' +
+                            '<ins class="adsbygoogle"\n' +
+                            '     style="display:block"\n' +
+                            '     data-ad-client="ca-pub-1632668592742327"\n' +
+                            '     data-ad-slot="3568893396"\n' +
+                            '     data-ad-format="auto"></ins>\n' +
+                            '</div>' +
+                            '<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>'
                         },
                         {
                             match: /<!-- ads-script -->/g,
-                            replacement: '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>'
+                            replacement: '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>'
                         }
                     ]
                 },
