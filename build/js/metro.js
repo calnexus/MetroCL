51362f69ab3b6b6c7c5202c946ca2767d55c7c68
Sergey Pimenov <sergey@pimenov.com.ua>
2018-01-02 13:59:54 +0200

    add cube

--- build/js/metro.js
+++ build/js/metro.js
@@ -1,6 +1,6 @@
 /*!
  * Metro 4 Components Library v4.0.0 (https://metroui.org.ua)
- * Copyright 2017 Sergey Pimenov
+ * Copyright 2018 Sergey Pimenov
  * Licensed under MIT
  */
 
@@ -5201,6 +5201,76 @@ var Countdown = {
 };
 
 Metro.plugin('countdown', Countdown);
+// Source: js/plugins/cube.js
+var Cube = {
+    init: function( options, elem ) {
+        this.options = $.extend( {}, this.options, options );
+        this.elem  = elem;
+        this.element = $(elem);
+
+        this._setOptionsFromDOM();
+        this._create();
+
+        return this;
+    },
+
+    options: {
+        cells: 4,
+        onCubeCreate: Metro.noop
+    },
+
+    _setOptionsFromDOM: function(){
+        var that = this, element = this.element, o = this.options;
+
+        $.each(element.data(), function(key, value){
+            if (key in o) {
+                try {
+                    o[key] = $.parseJSON(value);
+                } catch (e) {
+                    o[key] = value;
+                }
+            }
+        });
+    },
+
+    _create: function(){
+        var that = this, element = this.element, o = this.options;
+
+        this._createCube();
+
+        Utils.exec(o.onCubeCreate, [element]);
+    },
+
+    _createCube: function(){
+        var that = this, element = this.element, o = this.options;
+        var sides = ['left-side', 'right-side', 'top-side'];
+
+        element.addClass("cube");
+
+        $.each(sides, function(){
+            var side, cell, clsSide = this, i;
+            side = $("<div>").addClass("side " + clsSide).appendTo(element);
+            for(i = 0; i < Math.pow(o.cells, 2); i++) {
+                cell = $("<div>").addClass("cube-cell").appendTo(side);
+            }
+        });
+
+        setInterval(function(){
+            var cells = element.find(".cube-cell");
+            var cell_index = Utils.random(0, cells.length-1);
+            var cell = $(cells.get(cell_index)).addClass("light");
+            setTimeout(function(){
+                cell.removeClass("light");
+            }, 10000)
+        }, 1000);
+    },
+
+    changeAttribute: function(attributeName){
+
+    }
+};
+
+Metro.plugin('cube', Cube);
 // Source: js/plugins/datepicker.js
 var Datepicker = {
     init: function( options, elem ) {
